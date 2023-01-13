3f32afbc45694bca5754a616a72c42f15a91ab90
Sagar <jangidsagar1614@gmail.com>
2023-01-13 18:06:33 +0530

    Submitting Sucessfully

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5, 4);
+        p.product(5, 4, 6);
+        p.product(5.0, 4.0);
+    }
 
+    public static class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 }
+
+
